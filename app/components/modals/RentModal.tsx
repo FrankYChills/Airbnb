c7400a44c048cc24b65a9177a8f20a6bfbe61f0a
franky <itskchills11@gmail.com>
2023-04-20 10:32:02 +0530

    ameneties counter added

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -9,6 +9,7 @@ import CategoryInput from "../inputs/CategoryInput";
 import { FieldValues, useForm } from "react-hook-form";
 import CountrySelect from "../inputs/CountrySelect";
 import dynamic from "next/dynamic";
+import Counter from "../inputs/Counter";
 // import Map from "../Map";  -> to be imported in different way
 
 const RentModal = () => {
@@ -51,6 +52,9 @@ const RentModal = () => {
   const location = watch("location");
   console.log("cat-", category);
   console.log("loc-", location);
+  const guestCount = watch("guestCount"); //default 1 as defined in react form above
+  const roomCount = watch("roomCount");
+  const bathroomCount = watch("bathroomCount");
 
   // map
   // re import he map with the new location when location changes in country select
@@ -130,6 +134,37 @@ const RentModal = () => {
           value={location}
         />
         <Map center={location?.latlng} />
+      </div>
+    );
+  }
+
+  if (step === STEPS.INFO) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Share some basics about your place"
+          subtitle="What amenities do you have"
+        />
+        <Counter
+          title="Guests"
+          subtitle="How many guests do you allow?"
+          value={guestCount}
+          onChange={(value) => setCustomValue("guestCount", value)}
+        />
+        <hr />
+        <Counter
+          title="Rooms"
+          subtitle="How many rooms do you have?"
+          value={roomCount}
+          onChange={(value) => setCustomValue("roomCount", value)}
+        />
+        <hr />
+        <Counter
+          title="Bathrooms"
+          subtitle="How many bathrooms do you have?"
+          value={bathroomCount}
+          onChange={(value) => setCustomValue("bathroomCount", value)}
+        />
       </div>
     );
   }
