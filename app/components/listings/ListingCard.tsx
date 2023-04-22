52921072c22b15032f66259f33483a66d8388475
franky <itskchills11@gmail.com>
2023-04-22 12:48:15 +0530

    listing card UI

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -7,6 +7,8 @@ import { useRouter } from "next/navigation";
 import React, { useCallback, useMemo } from "react";
 import format from "date-fns/format";
 import Image from "next/image";
+import HeartButton from "../HeartButton";
+import Button from "../Button";
 
 interface ListingCardProps {
   data: Listing;
@@ -66,14 +68,34 @@ const ListingCard: React.FC<ListingCardProps> = ({
     >
       <div className="flex flex-col gap-2 w-full">
         <div className="aspect-square w-full relative overflow-hidden rounded-xl">
-          \
           <Image
             fill
             alt="Listing"
             src={data.imageSrc}
             className="object-cover h-full w-full group-hover:scale-110 transition"
           />
+          <div className="absolute top-3 right-3">
+            <HeartButton listingId={data.id} currentUser={currentUser} />
+          </div>
         </div>
+        <div className="font-semibold text-lg">
+          {location?.region}, {location?.label}
+        </div>
+        <div className="font-light text-neutral-600">
+          {reservationDate || data.category}
+        </div>
+        <div className="flex flex-row items-center gap-1">
+          <div className="font-semibold">$ {price}</div>
+          {!reservation && <div className="font-light">night</div>}
+        </div>
+        {onAction && actionLabel && (
+          <Button
+            disabled={disabled}
+            small
+            label={actionLabel}
+            onClick={handleCancel}
+          />
+        )}
       </div>
     </div>
   );
