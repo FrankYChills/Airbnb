9100eeaed2470599fa11e5e97d382850464b0d23
franky <itskchills11@gmail.com>
2023-04-22 12:00:59 +0530

    fetch listing

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default async function RootLayout({
         <RentModal />
         {/* <Modal isOpen title="Login" actionlabel="Submit" /> */}
         <Navbar currentUser={currentUser} />
-        {children}
+        <div className="pb-20 pt-20">{children}</div>
       </body>
     </html>
   );
