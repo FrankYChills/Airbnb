329a10b9388efe4e9ed2324b37ef0f1fab260108
franky <itskchills11@gmail.com>
2023-04-25 12:30:43 +0530

    added filters for listings

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -2,14 +2,74 @@ import prisma from "../libs/prismadb";
 
 export interface IListingsParams {
   userId?: string;
+  guestCount?: number;
+  roomCount?: number;
+  bathroomCount?: number;
+  startDate?: string;
+  endDate?: string;
+  locationValue?: string;
+  category?: string;
 }
 
-export default async function getListings({ userId }: IListingsParams) {
+export default async function getListings({
+  userId,
+  roomCount,
+  guestCount,
+  bathroomCount,
+  startDate,
+  endDate,
+  locationValue,
+  category,
+}: IListingsParams) {
   try {
     let query: any = {};
 
     if (userId) {
       query.userId = userId;
+    }
+
+    if (category) {
+      query.category = category;
+    }
+
+    if (roomCount) {
+      query.roomCount = {
+        gte: +roomCount, // rooms >= room count
+      };
+    }
+    if (guestCount) {
+      query.guestCount = {
+        gte: +guestCount,
+      };
+    }
+    if (bathroomCount) {
+      query.bathroomCount = {
+        gte: +bathroomCount,
+      };
+    }
+
+    if (locationValue) {
+      query.locationValue = locationValue;
+    }
+
+    if (startDate && endDate) {
+      // do not show the listing in which flitered startDate or enddate fall in a reserved listing start and enddate
+      query.NOT = {
+        reservations: {
+          some: {
+            OR: [
+              {
+                endDate: { gte: startDate },
+                startDate: { lte: startDate },
+              },
+              {
+                startDate: { lte: endDate },
+                endDate: { gte: endDate },
+              },
+            ],
+          },
+        },
+      };
     }
 
     const listings = await prisma.listing.findMany({
